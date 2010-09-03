02a78d9683961b049a0f087d03e5691e3fe4de3b
Jitka Novotna <copcas@gmail.com>
2010-09-03 13:10:55 +0200

    porad padada..

--- pixbuffer.c
+++ pixbuffer.c
@@ -1,15 +1,12 @@
 #include <stdlib.h> //calloc
-//#include <gdk-pixbuf/gdk-pixbuf.h>
 #include "pixbuffer.h"
 #include "poppler.h"
 
 int max_cached = 30;
 
 void really_free(struct pixbuf_pixbuf *s){
-//	g_free(s.pixbuf);
-//	gdk_pixbuf_finalize(s.pixbuf);
-	if (s->state != DONT_EXIST) g_object_unref(s->pixbuf);
-//	   gdk_pixbuf_unref                    (s.pixbuf);
+	if (s->state != DONT_EXIST)
+		g_object_unref(s->pixbuf);
 	s->state = DONT_EXIST;
 	s->width = 0;
 	s->height = 0;
@@ -18,14 +15,13 @@ void really_free(struct pixbuf_pixbuf *s){
 void pixbuf_create_database(pixbuf_database * database, int number_pages){
 	database->number_pages = number_pages;
 	database->page = calloc(number_pages, sizeof(struct pixbuf_pixbuf));
-	database->cached = calloc(number_pages, sizeof(int));
+	database->cached = calloc(max_cached, sizeof(int));
 	database->cached_length = 0;
 }
 
 void pixbuf_delete_database(pixbuf_database * old_db){
 	for (int i=0; i < old_db->number_pages; i++)
-//		if (old_db->page[i].state != DONT_EXIST) 
-			really_free(&old_db->page[i]);
+		really_free(&old_db->page[i]);
 	free(old_db->page);
 	free(old_db->cached);
 }
@@ -36,13 +32,12 @@ void pixbuf_render(pixbuf_database * database, int page, int width, int height, 
 
 	switch (db->state){
 		case CACHED:
-			//pomale netestovane
-			for (i=0; i<database->cached_length; i++)
+/*			for (i=0; i < database->cached_length; i++)
 				if (database->cached[i]==page)
 					break;
-			for (; i<database->cached_length; i++)
+			for (; i<database->cached_length-1; i++)
 				database->cached[i] = database->cached[i+1];
-		case USED:
+*/		case USED:
 			if (db->width == width && db->height == height && db->rotation == rotation)
 				return;
 			really_free(db);
@@ -54,7 +49,7 @@ void pixbuf_render(pixbuf_database * database, int page, int width, int height, 
 			pdf_render_page_to_pixbuf(
 					&database->page[page].pixbuf,
 					page, //int num_page
-					database->page[page].width,database->page[page].height, //int width, int height
+					width, height, //int width, int height
 					scale, //double scale
 					rotation); //int rotation);
 
@@ -67,12 +62,12 @@ void pixbuf_free(pixbuf_database * database, int number_page){
 	database->page[ number_page ].state = CACHED;
 	database->cached[ database->cached_length ] = number_page;
 	database->cached_length++;
- 	if (database->cached_length > max_cached){
-		int i = 0;
-		for (; i< max_cached/2; i++)
+/* 	if (database->cached_length >= max_cached){
+		for (int i=0; i < max_cached/2; i++)
 			really_free(&database->page[i]);
-		for (; i<number_page; i++)
+		for (int i=max_cached/2; i < max_cached; i++)
 			database->cached[i-max_cached/2]=database->cached[i];
 		database->cached_length -= max_cached/2;
 	}
+*/
 }
