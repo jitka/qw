c0c7948245370056bbefde358bfdf17da95d4443
Jitka Novotna <jitka@ucw.cz>
2010-09-21 12:30:17 +0200

    krabicka na pismenka je zase hezka

--- inputs.c
+++ inputs.c
@@ -14,23 +14,33 @@ int number;
 int click_x;
 int click_y;
 
-void wait_distance();
-void wait_position();
+void wait_distance(){ 	state = DISTANCE_1;}
+void wait_position(){ 	state = POSITION;}
 
-
-typedef struct command_char {
-	guint pressed_char;
-	void (*function)(void);
-} command_char;
-
-typedef struct number_char {
+struct {
+	unsigned int mode_mask;
 	guint pressed_char;
 	void (*function)(int);
-} number_char;
+} number_chars[] = {
+	//vzdy
+	{ ~0, GDK_p, key_jump },
+	{ ~0, GDK_P, key_jump },
+	{ ~0, GDK_Page_Down, key_jump_down },
+	{ ~0, GDK_Page_Up, key_jump_up },
+        { ~0, GDK_o, key_this_page_has_number },
+	//tabulka
+	{ PAGE|PRESENTATION, GDK_c, key_set_columns},
+	{ PAGE|PRESENTATION, GDK_l, key_set_rows},
+	{ PAGE|PRESENTATION, GDK_e, key_presentation_mode },
+};
 
-
+struct number_char{
+	guint pressed_char;
+	void (*function)(int);
+};
+	
 //pocislove pismenka
-static number_char number_chars[] = {
+static struct number_char number_chars2[] = {
 	{ GDK_p, key_jump },
 	{ GDK_P, key_jump },
 	{ GDK_Page_Down, key_jump_down },
@@ -41,37 +51,7 @@ static number_char number_chars[] = {
 	{ GDK_e, key_presentation_mode },
 };
 
-//jednopismenka pozor tohle se pusti opravdu _vzdy_
-static command_char basic_command_chars[] = {
-	{ GDK_space, key_down }, 	// dolu o 1 stranku
-	{ GDK_BackSpace, key_down },	// nahoru o 1 stranku
-	{ GDK_Home, key_home },
-	{ GDK_End, key_end },
-	{ GDK_a, key_rotate },
-	{ GDK_A, key_rotate_document },
-	{ GDK_q, key_quit },
-	{ GDK_F11, key_fullscreen },
-	{ GDK_f, key_fullscreen },
-	{ GDK_r, key_reload },
-	//klikaci pismenka
-	{ GDK_d, wait_distance },
-	{ GDK_D, wait_position },
-};
-static command_char page_command_chars[] = {
-	{ GDK_Right, key_down }, 		// dolu o 1 stranku
-	{ GDK_Left, key_up },			// nahoru o 1 stranku
-	{ GDK_Down, key_row_down },		// dolu o 1 radu
-	{ GDK_Up, key_row_up },			// nahoru o radu
-	{ GDK_Page_Down, key_screan_down },	// dolu o tabulku
-	{ GDK_Page_Up, key_screan_up },		// nahoru o tabulku
-	{ GDK_z, key_zoom_mode },
-	{ GDK_u, key_crop},
-};
-static command_char zoom_command_chars[] = {
-	{ GDK_Escape, key_page_mode },
-};
-
-struct {
+static struct {
 	unsigned int mode_mask;
 	guint pressed_char;
 	void (*function)(void);
@@ -103,17 +83,6 @@ struct {
 	{ ZOOM, GDK_Escape, key_page_mode },
 };
 
-void wait_distance(){ 	state = DISTANCE_1;}
-void wait_position(){ 	state = POSITION;}
-
-void aplicate_function(guint keyval, command_char *arr, int count){
-	for (int i=0; i < count; i++)
-		if (arr[i].pressed_char == keyval) {
-			arr[i].function();
-			break;
-		}
-}
-
 void handling_key(guint keyval){
 	if (mode == PRESENTATION)
 		key_page_mode();
@@ -124,7 +93,8 @@ void handling_key(guint keyval){
 				return;
 			}
 			for (unsigned int i=0; i < sizeof(number_chars) / sizeof(number_chars[0]); i++)
-				if (number_chars[i].pressed_char == keyval) {
+				if (number_chars[i].pressed_char == keyval &&
+						(command_chars[i].mode_mask & mode) ) {
 					number_chars[i].function(number);
 					break;
 				}
@@ -137,26 +107,12 @@ void handling_key(guint keyval){
 				return;
 			}
 
-/*			for (unsigned int i=0; i < sizeof(command_chars)/sizeof(command_chars[0]); i++)
+			for (unsigned int i=0; i < sizeof(command_chars)/sizeof(command_chars[0]); i++)
 				if ( command_chars[i].pressed_char == keyval &&
-						(command_chars[i].mode_mask & mode)
-						) {
+						(command_chars[i].mode_mask & mode) ) {
 					command_chars[i].function();
 					break;
 				}		
-*/			aplicate_function(keyval,basic_command_chars,sizeof(basic_command_chars)/sizeof(basic_command_chars[0]));
-			switch(mode){
-				case START:
-					break;
-				case PRESENTATION:
-					break;
-				case PAGE:
-					aplicate_function(keyval,page_command_chars,sizeof(page_command_chars)/sizeof(page_command_chars[0]));
-					break;
-				case ZOOM:
-					aplicate_function(keyval,zoom_command_chars,sizeof(zoom_command_chars)/sizeof(zoom_command_chars[0]));
-					break;
-			}
 			break;
 		default:
 			if (key_cancel_waiting)
