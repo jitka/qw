472daf3873a98ab372b78e5c5994b85a5c78bcb9
Jitka Novotna <jitka@ucw.cz>
2010-09-27 13:00:18 +0200

    pixbuf database

--- pixbuffer.c
+++ pixbuffer.c
@@ -12,7 +12,8 @@ void pixbuf_create_database(pixbuf_database * database){
 void pixbuf_free(gpointer data, gpointer size){
 	pixbuf_item *item = data;
 	*((int *) size) -= item->width * item->height;
-	g_object_unref(item->pixbuf);
+	if (item->pixbuf != NULL)
+		g_object_unref(item->pixbuf);
 	free(item);
 }
 void pixbuf_delete_database(pixbuf_database *db){
@@ -20,20 +21,29 @@ void pixbuf_delete_database(pixbuf_database *db){
 	g_list_free(db->glist);
 	db->glist = NULL;
 }
+void pixbuf_free_from_database(pixbuf_database *db,GList *old){
+	db->glist = g_list_remove_link(db->glist,old);
+	pixbuf_free(old->data,&db->size);
+	g_list_free_1(old);	
+}
+
+void pixbuf_insert_to_database(pixbuf_database *db, pixbuf_item *item){	
+	db->glist = g_list_append(db->glist,item);
+	db->size += item->width * item->height;
+}
 
 void pixbuf_delete_displayed(pixbuf_database *cache, pixbuf_item *arr, int length){
 	for(int i=0; i<length; i++){
 		pixbuf_item *tmp = malloc(sizeof(pixbuf_item));
 		memcpy(tmp,&arr[i],sizeof(pixbuf_item)); //kopiruje malou strukturu s ukazatelem na pixbuf
-		cache->glist = g_list_append(cache->glist,tmp);
-		cache->size += tmp->width * tmp->height;
+		pixbuf_insert_to_database(cache,tmp);	
+	//	cache->glist = g_list_append(cache->glist,tmp);
+	//	cache->size += tmp->width * tmp->height;
 	}
 	while (cache->size > max_size_of_cache){
 		//smaze první polozku
 		GList *first = g_list_first(cache->glist);
-		cache->glist = g_list_remove_link(cache->glist,first);
-		pixbuf_free(first->data,&cache->size);
-		g_list_free_1(first);
+		pixbuf_free_from_database(cache,first);
 	}
 }
 
