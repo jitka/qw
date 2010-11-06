ab75439779c530fccff064d7b4ef6f3869c8169a
Jitka Novotna <jitka@ucw.cz>
2010-11-06 20:39:12 +0100

    konecne spraven bug s predelavanim stranek

--- pixbuffer.c
+++ pixbuffer.c
@@ -3,35 +3,47 @@
 #include "pixbuffer.h"
 #include "backend.h"
 #include "settings.h"
-#include "window.h" //fuj - vizual
 
+#include "window.h" //fuj - vizual
 #include "poppler.h" //fuj
+
+void pixbuf_free(gpointer data, gpointer user_data){
+	pixbuf_item *item = data;
+	user_data = NULL;
+	if (item->pixbuf != NULL)
+		g_object_unref(item->pixbuf);
+	free(item);
+}
+
+
 void pixbuf_create_database(pixbuf_database * database){
 	database->glist = NULL;
 	database->size = 0;
 }
-
-void pixbuf_free(gpointer data, gpointer size){
-	pixbuf_item *item = data;
-	*((int *) size) -= item->width * item->height;
-	//if (item->pixbuf != NULL)
-		g_object_unref(item->pixbuf);
-	free(item);
-}
 void pixbuf_delete_database(pixbuf_database *db){
-	g_list_foreach(db->glist,pixbuf_free,&db->size);
+	g_list_foreach(db->glist,pixbuf_free,NULL);
 	g_list_free(db->glist);
 	db->glist = NULL;
+	db->size = 0;
+}
+
+
+pixbuf_item* pixbuf_remove_from_database(pixbuf_database *db,GList *old){
+	db->glist = g_list_remove_link(db->glist,old);
+	pixbuf_item *it = old->data;
+	db->size -= it->width * it->height; 
+	g_list_free_1(old);
+	return it;
 }
 void pixbuf_free_from_database(pixbuf_database *db,GList *old){
-	db->glist = g_list_remove_link(db->glist,old);
-	pixbuf_free(old->data,&db->size);
-	g_list_free_1(old);	
+	pixbuf_item *it = pixbuf_remove_from_database(db,old);
+	pixbuf_free(it,NULL);
 }
 void pixbuf_insert_into_database(pixbuf_database *db, pixbuf_item *item){	
 	db->glist = g_list_append(db->glist,item);
 	db->size += item->width * item->height;
 }
+
 
 void pixbuf_delete_displayed(pixbuf_database *cache, pixbuf_database *displayed){
 	cache->glist = g_list_concat(cache->glist,displayed->glist);
@@ -69,8 +81,9 @@ void pixbuf_render(pixbuf_database *cache,pixbuf_item *it){
 
 	GList *tmp = g_list_find_custom(cache->glist,it,compare);
 	if (tmp != NULL){ // pokud byla v cachy vynda se
-		cache->glist = g_list_remove_link(cache->glist,tmp);
-		memcpy(it,tmp->data,sizeof(pixbuf_item));
+		pixbuf_item *old = pixbuf_remove_from_database(cache,tmp);
+		it->pixbuf=old->pixbuf;
+		free(old);
 	} else { // jinak se vykresli
 		pdf_render_page_to_pixbuf(
 			&it->pixbuf,
@@ -80,4 +93,3 @@ void pixbuf_render(pixbuf_database *cache,pixbuf_item *it){
 			it->rotation);
 	}
 }
-
